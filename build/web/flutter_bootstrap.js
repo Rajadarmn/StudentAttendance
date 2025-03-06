2c32f8ed0f8712f828e251eb6d6c5aadf75f794f
dombatersesat <benedkudat@gmail.com>
2025-03-06 11:01:56 +0700

    first commit

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3310335351"
+    serviceWorkerVersion: "1015335482"
   }
 });
